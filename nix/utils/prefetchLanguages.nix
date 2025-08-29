649834ea596ba71010ee8c9aab7b2f2cd6fceeb8
Nicolas Jeannerod <nicolas.jeannerod@tweag.io>
2025-08-29 16:57:34 +0200

    Reintroduce a pre-commit hook and document Nix API (#1047)
    * Break down `prefetchLanguages.nix` and merge into `nix/`
    
    * Reintroduce Git hook with prefetched grammar
    
    * Document all functions
    
    * Generate Nix API reference page in the Topiary book
    
    * Store Nix API documentation in the repo -- keep in sync
    
    * Fix Nix API documentation
    
    * Remove empty comment lines
    
    Co-authored-by: Yann Hamdaoui <yann.hamdaoui@tweag.io>
    
    * Comment on the purpose of the `generate-nix-documentation` script
    
    Co-authored-by: Yann Hamdaoui <yann.hamdaoui@tweag.io>
    
    * Replace `hello` in `deepSeq` Nix check trick
    
    Co-authored-by: Yann Hamdaoui <yann.hamdaoui@tweag.io>
    
    * Improve documentation; document `filterConfig`
    
    Co-authored-by: Yann Hamdaoui <yann.hamdaoui@tweag.io>
    
    * Replace `toNickelFile` by `toJSONFile`
    
    Co-authored-by: Yann Hamdaoui <yann.hamdaoui@tweag.io>
    
    ---------
    
    Co-authored-by: Yann Hamdaoui <yann.hamdaoui@tweag.io>

---
+++ nix/utils/prefetchLanguages.nix
@@ -0,0 +1,99 @@
+## Because of dynamic loading, Topiary plays poorly in the Nix sandbox. This
+## file introduces two utilities, `prefetchLanguages` and
+## `prefetchLanguagesFile` that transform a Topiary configuration into another
+## one where all the grammars have been pre-fetched and pre-compiled in Nix
+## derivations.
+
+{
+  lib,
+  fetchgit,
+  nickel,
+  runCommandNoCC,
+  writeText,
+  tree-sitter,
+  toJSONFile,
+  fromNickelFile,
+}:
+
+let
+  inherit (builtins)
+    attrNames
+    concatStringsSep
+    mapAttrs
+    toFile
+    readFile
+    toJSON
+    fromJSON
+    baseNameOf
+    ;
+  inherit (lib) warn;
+  inherit (lib.strings) removeSuffix;
+  inherit (lib.attrsets) updateManyAttrsByPath;
+
+  prefetchLanguageSourceGit =
+    name: source:
+    tree-sitter.buildGrammar {
+      language = name;
+      version = source.rev;
+      src = fetchgit {
+        url = source.git;
+        rev = source.rev;
+        hash =
+          if source ? "nixHash" then
+            source.nixHash
+          else
+            warn "Language `${name}`: no nixHash provided - using dummy value" "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+      };
+      location = if source ? "subdir" then source.subdir else null;
+    };
+
+  prefetchLanguageSource =
+    name: source:
+    if source ? "path" then
+      { inherit (source) path; }
+    else if source ? "git" then
+      { path = "${prefetchLanguageSourceGit name source.git}/parser"; }
+    else
+      throw ("Unsupported Topiary language sources: " ++ concatStringsSep ", " (attrNames source));
+
+  updateByPath = path: update: updateManyAttrsByPath [ { inherit path update; } ];
+
+  /**
+    Given a Topiary configuration as a Nix value, returns the same
+    configuration, except all language sources have been replaced by a
+    prefetched and precompiled one. This requires the presence of a `nixHash`
+    for all sources.
+
+    # Type
+
+    ```
+    prefetchLanguages : TopiaryConfig -> TopiaryConfig
+    ```
+  */
+  prefetchLanguages = updateByPath [ "languages" ] (
+    mapAttrs (name: updateByPath [ "grammar" "source" ] (prefetchLanguageSource name))
+  );
+
+  /**
+    Same as `prefetchLanguages`, but expects a path to a Nickel file, and
+    produces a path to a JSON file, which can be consumed by Nickel.
+
+    # Type
+
+    ```
+    prefetchLanguagesFile : File -> File
+    ```
+  */
+  prefetchLanguagesFile =
+    topiaryConfigFile:
+    toJSONFile "${removeSuffix ".ncl" (baseNameOf topiaryConfigFile)}-prefetched.json" (
+      prefetchLanguages (fromNickelFile topiaryConfigFile)
+    );
+
+in
+{
+  inherit
+    prefetchLanguages
+    prefetchLanguagesFile
+    ;
+}
